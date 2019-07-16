b2b8d5e871c5266a2d1e8dcf5477890be614eeb4
VasAngels <angelis.vassilis@gmail.com>
2019-07-16 09:01:28 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require ('express')
+const cron = require('node-cron')
+const bodyParser = require('body-parser')
+const request =  require('request')
+const {scheduler} = require('./helpers')
+const {sendEmail} = require('./helpers')
+
+const port = process.env.PORT || 8080;
+const app = new express();
+
+var counter = 4;                        
+const emailList = ['Tim.Andrews@open.ac.uk', 'Vassilis.Angelis@open.ac.uk','James.Barnett@open.ac.uk', 'Sara.Grabauskas@open.ac.uk', 'Tushar.Pai@open.ac.uk',
+'Adriano.Perrone@open.ac.uk','Patrick.Sim@open.ac.uk', 'Dale.Singh@open.ac.uk', 'Clem.Whiting@open.ac.uk', 'Richard.Willmoth@open.ac.uk']
+
+
+cron.schedule(' */20 * * * * 1-5', ()=> {
+    scheduler(counter,emailList)
+})
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log('the app is listening in port 3000')
+})
---
+++ helpers/index.js
@@ -0,0 +1,46 @@
+//////////sendEmail
+const nodeMailer = require("nodemailer");
+ 
+const defaultEmailData = { from: "noreply@node-react.com" };
+ 
+sendEmail = emailData => {
+    const transporter = nodeMailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: "pass.reset.req@gmail.com",
+            pass: "zsbj ftmy qamf zjmf"
+        }
+    });
+    return (
+        transporter
+            .sendMail(emailData)
+            .then(info => console.log(`Message sent: ${info.response}`))
+            .catch(err => console.log(`Problem sending email: ${err}`))
+    );
+};
+ 
+ ////////Scheduler
+ exports.scheduler = (counter,emailList)=>{
+  if (counter===10){counter=0}
+
+  var currentDay = new Date();
+  var today = currentDay.getDay();
+  // const email = emailList[counter]
+  const email = 'angelis.vassiliis@gmail.com'
+
+
+  const emailData={
+    from:"noreply@node-react.com",
+    to:email,
+    subject: "DART turn reminder",
+    text: "It's your turn to keep an eye on DART requests today",
+    html: "<p>It's your turn to keep an eye on DART requests today</p>"
+  }
+sendEmail(emailData)
+console.log(counter)
+counter ++
+
+console.log('Email is sent')}

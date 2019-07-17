95e021f4e334858c4bb5b50ede71d24d21d49fdc
VasAngels <angelis.vassilis@gmail.com>
2019-07-17 21:26:13 +0300

    deployment v 2 scheduled job added on heroku

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const {sendEmail} = require('./helpers')
 const port = process.env.PORT || 8080;
 const app = new express();
 
-var counter = 2;                        
+var counter = 3;                        
 const emailList = ['Tim.Andrews@open.ac.uk', 'Vassilis.Angelis@open.ac.uk','James.Barnett@open.ac.uk', 'Sara.Grabauskas@open.ac.uk', 'Tushar.Pai@open.ac.uk',
 'Adriano.Perrone@open.ac.uk','Patrick.Sim@open.ac.uk', 'Dale.Singh@open.ac.uk', 'Clem.Whiting@open.ac.uk', 'Richard.Willmoth@open.ac.uk']
 
 
-cron.schedule(' 0 7 * * 1-5', ()=> {
+cron.schedule(' 15 7 * * 1-5', ()=> {
     scheduler(counter,emailList)
     counter++
 },{timezone: "Europe/London"})

c28251fa7b243682c799db2624550fcd9c696de4
VasAngels <angelis.vassilis@gmail.com>
2019-07-19 06:43:01 +0100

    deployment v3 counter changed to 4

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const {sendEmail} = require('./helpers')
 const port = process.env.PORT || 8080;
 const app = new express();
 
-var counter = 3;                        
+var counter = 4;                        
 const emailList = ['Tim.Andrews@open.ac.uk', 'Vassilis.Angelis@open.ac.uk','James.Barnett@open.ac.uk', 'Sara.Grabauskas@open.ac.uk', 'Tushar.Pai@open.ac.uk',
 'Adriano.Perrone@open.ac.uk','Patrick.Sim@open.ac.uk', 'Dale.Singh@open.ac.uk', 'Clem.Whiting@open.ac.uk', 'Richard.Willmoth@open.ac.uk']
 
